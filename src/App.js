ae693ce49113f3c46879cf167eb65428b784efd5
BrunoMatosDev <sotamenterprise@gmail.com>
2023-12-19 12:45:37 +0000

    Revert "Add Footer"
    This reverts commit 2b86a5baff518a39dca2b270938f77f3aad20d1e.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import Portfolio from "./components/Portfolio";
 import SocialLinks from "./components/SocialLinks";
-import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -17,7 +16,6 @@ function App() {
       <Experience />
       <Contact />
       <SocialLinks />
-      <Footer />
     </div>
   );
 }
